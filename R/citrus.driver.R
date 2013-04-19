cf48c865788084ede2ae9f9d8303df38c72b7856
Robert Bruggner <rbruggner@gmail.com>
2013-04-19 15:53:04 -0700

    Returning more information from results.

--- R/citrus.driver.R
+++ R/citrus.driver.R
@@ -172,7 +172,7 @@ citrus.endpointRegress = function(preclusterResult,outputDir,family,labels,model
     differentialFeatures
     
     if (returnResults){
-      regressionRes[[conditionName]] = list(foldLargeEnoughClusters=foldLargeEnoughClusters,foldFeatures=foldFeatures,differentialFeatures=differentialFeatures)  
+      regressionRes[[conditionName]] = list(foldLargeEnoughClusters=foldLargeEnoughClusters,foldFeatures=foldFeatures,differentialFeatures=differentialFeatures,cvMinima=cvMinima,thresholdCVRates=thresholdCVRates,foldModels=foldModels,regularizationThresholds=regularizationThresholds)  
     }
     
     # Plot
