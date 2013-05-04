6ded80737d159b6e4643926138535944921c8f07
rbruggner <rbruggner@gmail.com>
2013-05-04 15:28:13 -0700

    Moving preCluster to cluster.R file. Adding snow support for cluster assignment.

--- R/citrus.cluster.R
+++ R/citrus.cluster.R
@@ -53,3 +53,63 @@ citrus.traverseMergeOrder = function(node,mergeOrder){
   }
   return(c(leftAtom,rightAtom));
 }
+
+
+citrus.preCluster = function(dataDir,outputDir,clusterCols,fileSampleSize,fileList,nFolds,transformCols=NULL,conditionComparaMatrix=NULL,balanceFactor=NULL,transformFactor=5,...){
+  
+  addtlArgs = list(...)
+  
+  res=list()
+  if (!file.exists(outputDir)){
+    stop(paste("Output directory",outputDir,"not found."))
+  }
+  
+  if (!is.null(conditionComparaMatrix)){
+    allConditions = citrus.convertConditionMatrix(conditionComparaMatrix) 
+  } else {
+    allConditions = as.list(colnames(fileList))
+  }
+  
+  if (is.null(balanceFactor)){
+    balanceFactor = as.factor(sample(c(0,1),nrow(fileList),replace=T))
+  } else if (levels(balanceFactor)==1){
+    balanceFactor = as.factor(sample(c(0,1),length(balanceFactor),replace=T))
+  }
+  if (nFolds=="all"){
+    folds = list()
+    nAllFolds=1
+  } else {
+    folds = pamr:::balanced.folds(y=balanceFactor,nfolds=nFolds)
+    nAllFolds = nFolds+1  
+  }
+  folds[[nAllFolds]]="all"
+  
+  for (conditions in allConditions){
+    cat(paste("Clustering Condition",paste(conditions,collapse=" vs "),"\n"))
+    
+    citrus.dataArray = citrus.readFCSSet(dataDir=dataDir,fileList=fileList,conditions=conditions,transformCols=transformCols,fileSampleSize=fileSampleSize,transformFactor=transformFactor)
+    
+    foldsCluster = lapply(folds,citrus.foldCluster,citrus.dataArray=citrus.dataArray,clusterCols=clusterCols,conditions=conditions)
+    cat("Assigning Events to Clusters\n")
+    if ("snowCluster" %in% names(addtlArgs)){
+      foldsClusterAssignments = parLapply(addtlArgs[["snowCluster"]],foldsCluster,citrus.calculateCompleteHierarchicalMembership)
+    } else {
+      foldsClusterAssignments = lapply(foldsCluster,citrus.calculateCompleteHierarchicalMembership)  
+    }
+    
+    resObj = list(folds=folds,foldsCluster=foldsCluster,foldsClusterAssignments=foldsClusterAssignments,conditions=conditions,citrus.dataArray=citrus.dataArray,clusterColumns=clusterCols)
+    if (nFolds!="all"){
+      cat("Assigning Leftout Events to Clusters\n")
+      if ("snowCluster" %in% names(addtlArgs)){
+        leftoutClusterAssignments = parLapply(addtlArgs[["snowCluster"]],1:nFolds,citrus.mapFoldDataToClusterSpace,citrus.dataArray=citrus.dataArray,foldClusterAssignments=foldsClusterAssignments,folds=folds,conditions=conditions,clusterCols=clusterCols)
+      } else {
+        leftoutClusterAssignments = lapply(1:nFolds,citrus.mapFoldDataToClusterSpace,citrus.dataArray=citrus.dataArray,foldClusterAssignments=foldsClusterAssignments,folds=folds,conditions=conditions,clusterCols=clusterCols)  
+      }
+      
+      resObj$leftoutClusterAssignments=leftoutClusterAssignments
+    }
+    save(resObj,file=file.path(outputDir,paste("citrus.Cluster.",paste(conditions,collapse="_vs_"),".rDat",sep="")))
+    res[[paste(conditions,collapse="_vs_")]]=resObj
+  }
+  return(res)
+}
--- R/citrus.driver.R
+++ R/citrus.driver.R
@@ -51,53 +51,6 @@ citrus.quick = function(dataDir,outputDir,clusterCols,fileSampleSize,fileList,la
 }
 
 
-citrus.preCluster = function(dataDir,outputDir,clusterCols,fileSampleSize,fileList,nFolds,transformCols=NULL,conditionComparaMatrix=NULL,balanceFactor=NULL,transformFactor=5){
-  
-  res=list()
-  if (!file.exists(outputDir)){
-    stop(paste("Output directory",outputDir,"not found."))
-  }
-  
-  if (!is.null(conditionComparaMatrix)){
-    allConditions = citrus.convertConditionMatrix(conditionComparaMatrix) 
-  } else {
-    allConditions = as.list(colnames(fileList))
-  }
-  
-  if (is.null(balanceFactor)){
-    balanceFactor = as.factor(sample(c(0,1),nrow(fileList),replace=T))
-  } else if (levels(balanceFactor)==1){
-    balanceFactor = as.factor(sample(c(0,1),length(balanceFactor),replace=T))
-  }
-  if (nFolds=="all"){
-    folds = list()
-    nAllFolds=1
-  } else {
-    folds = pamr:::balanced.folds(y=balanceFactor,nfolds=nFolds)
-    nAllFolds = nFolds+1  
-  }
-  folds[[nAllFolds]]="all"
-  
-  for (conditions in allConditions){
-    cat(paste("Clustering Condition",paste(conditions,collapse=" vs "),"\n"))
-    
-    citrus.dataArray = citrus.readFCSSet(dataDir=dataDir,fileList=fileList,conditions=conditions,transformCols=transformCols,fileSampleSize=fileSampleSize,transformFactor=transformFactor)
-    
-    foldsCluster = lapply(folds,citrus.foldCluster,citrus.dataArray=citrus.dataArray,clusterCols=clusterCols,conditions=conditions)
-    cat("Assigning Events to Clusters\n")
-    foldsClusterAssignments = lapply(foldsCluster,citrus.calculateCompleteHierarchicalMembership)
-    resObj = list(folds=folds,foldsCluster=foldsCluster,foldsClusterAssignments=foldsClusterAssignments,conditions=conditions,citrus.dataArray=citrus.dataArray,clusterColumns=clusterCols)
-    if (nFolds!="all"){
-      cat("Assigning Leftout Events to Clusters\n")
-      leftoutClusterAssignments = lapply(1:nFolds,citrus.mapFoldDataToClusterSpace,citrus.dataArray=citrus.dataArray,foldClusterAssignments=foldsClusterAssignments,folds=folds,conditions=conditions,clusterCols=clusterCols)
-      resObj$leftoutClusterAssignments=leftoutClusterAssignments
-    }
-    save(resObj,file=file.path(outputDir,paste("citrus.Cluster.",paste(conditions,collapse="_vs_"),".rDat",sep="")))
-    res[[paste(conditions,collapse="_vs_")]]=resObj
-  }
-  return(res)
-}
-
 citrus.endpointRegress.quick = function(preclusterResult,outputDir,family,labels,modelTypes=c("pamr","glmnet"),featureTypes=c("densities"),minimumClusterSizePercent=0.05,plot=T,returnResults=F,...){
   
   addtlArgs = list(...)
