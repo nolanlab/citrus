79a2e248656d116e0191dab094b7ae557e7ecd82
rbruggner <rbruggner@gmail.com>
2013-10-08 13:46:49 -0700

    Updating way citruskey is parsed. Also turning off multithreading disabling for mac os x as that should be controlled by the package installation.

--- inst/shinyGUI/global.R
+++ inst/shinyGUI/global.R
@@ -2,44 +2,45 @@ runCitrus = FALSE;
 
 preload=F
 # Choose any file from the appropriate directory
+
 if (!exists("dataDir")){
   dataDirFile = file.choose()
   if (is.null(dataDirFile)){
     stop("File Selection Canceled")
-  } else {
-	  if (basename(dataDirFile)=="citruskey.csv"){
-		  tryCatch({
-        keyFile = read.csv(dataDirFile,header=T)
-      }, warning = function(w) {
-        stop(simpleWarning(paste("Error Reading input file:",w)))
-      }, error = function(e) {
-        stop(simpleError(paste("Error Reading input file:",e)))
-      }, finally = {
-        if (!("labels" %in% colnames(keyFile))){
-          stop("Error reading citrus key: 'labels' column not found");
-        } else {
-          labelCol = which(colnames(keyFile)=="labels")
-        }
-        preload=T
-      });
-	  }
-    
-    dataDir = dirname(dataDirFile)
-  }  
+  }
 } else {
-	if (!file.exists(dataDir)){
-	  stop(paste("Directory",dataDir,"not found. Exiting."))
-	}
+  if (!file.exists(dataDir)){
+    stop(paste("Directory",dataDir,"not found. Exiting."))
+  }
+  dataDirFile = dataDir;
 }
 
+if (basename(dataDirFile)=="citruskey.csv"){
+  tryCatch({
+    keyFile = read.csv(dataDirFile,header=T)
+  }, warning = function(w) {
+    stop(simpleWarning(paste("Error Reading input file:",w)))
+  }, error = function(e) {
+    stop(simpleError(paste("Error Reading input file:",e)))
+  }, finally = {
+    if (!("labels" %in% colnames(keyFile))){
+      stop("Error reading citrus key: 'labels' column not found");
+    } else {
+      labelCol = which(colnames(keyFile)=="labels")
+    }
+    preload=T
+    dataDir = dirname(dataDirFile)
+  });
+}
+    
 cat(paste("Launching citrus interface with target directory:",dataDir,"\n"));
 
 # Lousy Fix to keep open mp processes from dying in mac os x
-if (Sys.info()[["sysname"]]=="Darwin"){
-  cat("Mac OS X Detected. Setting number of OpenMP threads to 1.\n")
-  Rclusterpp.setThreads(threads=1)
-  Sys.setenv("OMP_NUM_THREADS"=1)
-}
+#if (Sys.info()[["sysname"]]=="Darwin"){
+#  cat("Mac OS X Detected. Setting number of OpenMP threads to 1.\n")
+#  Rclusterpp.setThreads(threads=1)
+#  Sys.setenv("OMP_NUM_THREADS"=1)
+#}
 
 # Comment to True to debug
 options(shiny.trace=F)
