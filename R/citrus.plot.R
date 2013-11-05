df5edbfa487a007e9a3cdc629b548615eedb75a0
rbruggner <rbruggner@gmail.com>
2013-11-05 10:14:31 -0800

    Changing graph color palette again.

--- R/citrus.plot.R
+++ R/citrus.plot.R
@@ -323,12 +323,12 @@ citrus.plotHierarchicalClusterMedians = function(outputFile,clusterMedians,graph
   pdf(file=outputFile,width=15,height=15,bg=bg)
   for (target in 1:ncol(clusterMedians)){
     ct = seq(from=(min(clusterMedians[,target])-0.01),to=(max(clusterMedians[,target])+0.01),length.out=20)
-    cols = topo.colors(20)[sapply(clusterMedians[,target],findInterval,vec=ct)]
+    cols = .graphColorPalette(20)[sapply(clusterMedians[,target],findInterval,vec=ct)]
     par(col.main=stroke)  
     plot.igraph(graph,layout=layout,vertex.color=cols,main=colnames(clusterMedians)[target],edge.color=stroke,vertex.label.color="white",edge.arrow.size=.2,vertex.frame.color=strokea,vertex.label.cex=.7,vertex.label.family="Helvetica")
     
     # Legend
-    legend_image <- as.raster(matrix(rev(topo.colors(20)), ncol=1))
+    legend_image <- as.raster(matrix(rev(.graphColorPalette(20)), ncol=1))
     rasterImage(legend_image, 1.1, -.5, 1.15,.5)
     text(x=1.15, y = seq(-.5,.5,l=5), labels = .decimalFormat(ct[c(1,floor((length(ct)/4)*1:4))]) ,pos=4,col=stroke)
   }
@@ -364,10 +364,10 @@ citrus.plotHierarchicalClusterFeatureGroups = function(outputFile,featureCluster
         stop("clusterMedians argument must be supplied to plot petals")
       }
       add.vertex.shape("petal", clip=vertex.shapes("circle")$clip,plot=.petalVertex, parameters=list(vertex.scale=.04,vertex.weights=apply(clusterMedians,2,.scaleToOne)))
-      plot.igraph(graph,layout=layout,mark.groups=fGroup,mark.expand=5,main=feature,edge.color=stroke,vertex.label.color="white",edge.arrow.size=.2,vertex.frame.color=strokea,vertex.label.cex=.7,vertex.label.family="Helvetica",vertex.color=rgb(0,0,.5,.3),mark.col=topo.colors(length(fGroup),alpha=.3),vertex.shape="petal")
+      plot.igraph(graph,layout=layout,mark.groups=fGroup,mark.expand=5,main=feature,edge.color=stroke,vertex.label.color="white",edge.arrow.size=.2,vertex.frame.color=strokea,vertex.label.cex=.7,vertex.label.family="Helvetica",vertex.color=rgb(0,0,.5,.3),mark.col=.graphColorPalette(length(fGroup),alpha=.3),vertex.shape="petal")
       .petalPlot(xpos=1,ypos=-1,d=rep(1,ncol(clusterMedians)),scale=.2,labels=colnames(clusterMedians))
     } else {
-      plot.igraph(graph,layout=layout,mark.groups=fGroup,mark.expand=5,main=feature,edge.color=stroke,vertex.label.color="white",edge.arrow.size=.2,vertex.frame.color=strokea,vertex.label.cex=.7,vertex.label.family="Helvetica",vertex.color=rgb(0,0,.5,.5),mark.col=topo.colors(length(fGroup),alpha=.8))    
+      plot.igraph(graph,layout=layout,mark.groups=fGroup,mark.expand=5,main=feature,edge.color=stroke,vertex.label.color="white",edge.arrow.size=.2,vertex.frame.color=strokea,vertex.label.cex=.7,vertex.label.family="Helvetica",vertex.color=rgb(0,0,.5,.5),mark.col=.graphColorPalette(length(fGroup),alpha=.8))    
     }
   }
   dev.off()
@@ -424,5 +424,8 @@ citrus.plotRegressionResults = function(outputDir,citrus.preclusterResult,citrus
     }
     
   }
-  
+}
+
+.graphColorPalette(x){
+  rainbow(x,alpha=.8,start=.65,end=.18)
 }
