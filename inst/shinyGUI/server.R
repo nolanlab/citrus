5e800ca232bdabafbd4b8beaad05a8f8a9c47ac9
Robert Bruggner <rbruggner@gmail.com>
2014-10-31 00:06:57 -0700

    Updating documentation.

--- inst/shinyGUI/server.R
+++ inst/shinyGUI/server.R
@@ -155,6 +155,7 @@ shinyServer(function(input, output) {
     if ((length(names(selectedFiles))<2)||(!all(unlist(lapply(selectedFiles,length))>1))){
       return(tagList(tags$b("Assign samples to groups to enable specification of cross-validation folds")))
     } else {
+      print(paste("FILES:",nFiles))
       return(tagList(numericInput(inputId="crossValidationFolds",label="Cross Validation Folds",value=1,min=1,max=nFiles)))
     }
   })
